3a751e2bfde77367b3fccb39c5f10b2512c8b273
Tom Wright <contact@tomwright.me>
2020-09-22 15:22:27 +0100

    Remove some redundant tests

--- node_test.go
+++ node_test.go
@@ -1,11 +1,9 @@
 package dasel_test
 
 import (
-	"errors"
 	"fmt"
 	"github.com/tomwright/dasel"
 	"github.com/tomwright/dasel/internal/storage"
-	"reflect"
 	"testing"
 )
 
@@ -119,7 +117,7 @@ func TestNode_Query_File(t *testing.T) {
 	t.Run("YAML", fileTest("./tests/assets/example.yaml"))
 }
 
-func TestNode_Query(t *testing.T) {
+func TestNode_Query_Data(t *testing.T) {
 	t.Run("ParentChildPathToProperty", func(t *testing.T) {
 		rootNode := dasel.New(mapRoot)
 
@@ -170,144 +168,6 @@ func TestNode_Query(t *testing.T) {
 
 		if exp, got := "Tom", got.Value.(string); exp != got {
 			t.Errorf("expected %s, got %s", exp, got)
-		}
-	})
-
-	t.Run("Traversal", func(t *testing.T) {
-		tests := parseTest{
-			Selector: ".a.b.c.thing",
-			Input:    mapRoot,
-		}.Add(
-			"StartsAtEndElement",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".thing", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual("1", n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", "1", n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Previous1",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Previous, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".c", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual(mapC, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapC, n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Previous2",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Previous, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".b", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual(mapB, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapB, n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Previous3",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Previous, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".a", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual(mapA, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapA, n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Next",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Next, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".b", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-
-				if !reflect.DeepEqual(mapB, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapB, n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Previous4",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Previous, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".a", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual(mapA, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapA, n.Value)
-					return false
-				}
-				return true
-			},
-		).Add(
-			"Previous5",
-			func(t *testing.T, n *dasel.Node) (*dasel.Node, bool) { return n.Previous, true },
-			func(t *testing.T, n *dasel.Node) bool {
-				if exp, got := ".", n.Selector.Current; exp != got {
-					t.Errorf("expected selector of `%s`, got `%s`", exp, got)
-					return false
-				}
-				if !reflect.DeepEqual(mapRoot, n.Value) {
-					t.Errorf("expected value of\n%s\ngot\n%s", mapRoot, n.Value)
-					return false
-				}
-				return true
-			},
-		)
-
-		rootNode := dasel.New(tests.Input)
-		got, err := rootNode.Query(tests.Selector)
-		switch {
-		case err == nil && tests.Err != nil:
-			t.Errorf("expected err `%v`, got `%v`", tests.Err, err)
-			return
-		case err != nil && tests.Err == nil:
-			t.Errorf("unexpected err `%v`", err)
-			return
-		case err != nil && tests.Err != nil && !errors.Is(err, tests.Err):
-			t.Errorf("expected err `%v`, got `%v`", tests.Err, err)
-			return
-		}
-
-		ok := true
-
-		for i := 0; i < len(tests.ProcessFns); i++ {
-			if !ok {
-				break
-			}
-			t.Run(tests.Names[i], func(t *testing.T) {
-				got, ok = tests.ProcessFns[i](t, got)
-				if !ok {
-					return
-				}
-				ok = tests.CheckFns[i](t, got)
-				if !ok {
-					return
-				}
-			})
 		}
 	})
 }
