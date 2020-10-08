0c5a2c8ceb6a05096cbce437594abb085fec65c2
Tom Wright <contact@tomwright.me>
2020-10-08 16:52:31 +0100

    Add some put object tests

--- internal/command/root_put_test.go
+++ internal/command/root_put_test.go
@@ -52,6 +52,52 @@ func putTest(in string, varType string, parser string, selector string, value st
 	}
 }
 
+func putObjectTest(in string, parser string, selector string, values []string, types []string, out string, expErr error) func(t *testing.T) {
+	return func(t *testing.T) {
+		cmd := command.NewRootCMD()
+		outputBuffer := bytes.NewBuffer([]byte{})
+
+		args := []string{
+			"put", "object", "-p", parser, "-o", "stdout", "-s", selector,
+		}
+		for _, t := range types {
+			args = append(args, "-t", t)
+		}
+		args = append(args, values...)
+
+		cmd.SetOut(outputBuffer)
+		cmd.SetIn(strings.NewReader(in))
+		cmd.SetArgs(args)
+
+		err := cmd.Execute()
+
+		if expErr == nil && err != nil {
+			t.Errorf("expected err %v, got %v", expErr, err)
+			return
+		}
+		if expErr != nil && err == nil {
+			t.Errorf("expected err %v, got %v", expErr, err)
+			return
+		}
+		if expErr != nil && err != nil && err.Error() != expErr.Error() {
+			t.Errorf("expected err %v, got %v", expErr, err)
+			return
+		}
+
+		output, err := ioutil.ReadAll(outputBuffer)
+		if err != nil {
+			t.Errorf("unexpected error reading output buffer: %s", err)
+			return
+		}
+
+		out = strings.TrimSpace(out)
+		outputStr := strings.TrimSpace(string(output))
+		if out != outputStr {
+			t.Errorf("expected result %v, got %v", out, outputStr)
+		}
+	}
+}
+
 func putStringTest(in string, parser string, selector string, value string, out string, expErr error) func(t *testing.T) {
 	return putTest(in, "string", parser, selector, value, out, expErr)
 }
@@ -64,92 +110,182 @@ func putBoolTest(in string, parser string, selector string, value string, out st
 	return putTest(in, "bool", parser, selector, value, out, expErr)
 }
 
-func putStringTestForParserJSON(parser string) func(t *testing.T) {
+func putStringTestForParserJSON() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putStringTest(`{
   "id": "x"
-}`, parser, "id", "y", `{
+}`, "json", "id", "y", `{
   "id": "y"
 }`, nil))
 	}
 }
 
-func putIntTestForParserJSON(parser string) func(t *testing.T) {
+func putIntTestForParserJSON() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putIntTest(`{
   "id": 123
-}`, parser, "id", "456", `{
+}`, "json", "id", "456", `{
   "id": 456
 }`, nil))
 	}
 }
 
-func putBoolTestForParserJSON(parser string) func(t *testing.T) {
+func putBoolTestForParserJSON() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putBoolTest(`{
   "id": true
-}`, parser, "id", "false", `{
+}`, "json", "id", "false", `{
   "id": false
 }`, nil))
 	}
 }
 
-func putStringTestForParserYAML(parser string) func(t *testing.T) {
+func putObjectTestForParserJSON() func(t *testing.T) {
+	return func(t *testing.T) {
+
+		t.Run("OverwriteObject", putObjectTest(`{
+  "numbers": [
+    {
+      "rank": 1,
+      "number": "one"
+    },
+    {
+      "rank": 2,
+      "number": "two"
+    },
+    {
+      "rank": 3,
+      "number": "three"
+    }
+  ]
+}`, "json", ".numbers.[0]", []string{"number=five", "rank=5"}, []string{"string", "int"}, `{
+  "numbers": [
+    {
+      "number": "five",
+      "rank": 5
+    },
+    {
+      "number": "two",
+      "rank": 2
+    },
+    {
+      "number": "three",
+      "rank": 3
+    }
+  ]
+}`, nil))
+
+	}
+}
+
+func putStringTestForParserYAML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putStringTest(`
 id: "x"
-`, parser, "id", "y", `
+`, "yaml", "id", "y", `
 id: "y"
 `, nil))
 	}
 }
 
-func putIntTestForParserYAML(parser string) func(t *testing.T) {
+func putIntTestForParserYAML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putIntTest(`
 id: 123
-`, parser, "id", "456", `
+`, "yaml", "id", "456", `
 id: 456
 `, nil))
 	}
 }
 
-func putBoolTestForParserYAML(parser string) func(t *testing.T) {
+func putBoolTestForParserYAML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putBoolTest(`
 id: true
-`, parser, "id", "false", `
+`, "yaml", "id", "false", `
 id: false
 `, nil))
 	}
 }
 
-func putStringTestForParserTOML(parser string) func(t *testing.T) {
+func putObjectTestForParserYAML() func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Run("OverwriteObject", putObjectTest(`
+numbers:
+- number: one
+  rank: 1
+- number: two
+  rank: 2
+- number: three
+  rank: 3
+`, "yaml", ".numbers.[0]", []string{"number=five", "rank=5"}, []string{"string", "int"}, `
+numbers:
+- number: five
+  rank: 5
+- number: two
+  rank: 2
+- number: three
+  rank: 3
+`, nil))
+	}
+}
+
+func putStringTestForParserTOML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putStringTest(`
 id = "x"
-`, parser, "id", "y", `
+`, "toml", "id", "y", `
 id = "y"
 `, nil))
 	}
 }
 
-func putIntTestForParserTOML(parser string) func(t *testing.T) {
+func putIntTestForParserTOML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putIntTest(`
 id = 123
-`, parser, "id", "456", `
+`, "toml", "id", "456", `
 id = 456
 `, nil))
 	}
 }
 
-func putBoolTestForParserTOML(parser string) func(t *testing.T) {
+func putBoolTestForParserTOML() func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Property", putBoolTest(`
 id = true
-`, parser, "id", "false", `
+`, "toml", "id", "false", `
 id = false
+`, nil))
+	}
+}
+
+func putObjectTestForParserTOML() func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Run("OverwriteObject", putObjectTest(`
+[[numbers]]
+  number = "one"
+  rank = 1
+
+[[numbers]]
+  number = "two"
+  rank = 2
+
+[[numbers]]
+  number = "three"
+  rank = 3
+`, "toml", ".numbers.[0]", []string{"number=five", "rank=5"}, []string{"string", "int"}, `
+[[numbers]]
+  number = "five"
+  rank = 5
+
+[[numbers]]
+  number = "two"
+  rank = 2
+
+[[numbers]]
+  number = "three"
+  rank = 3
 `, nil))
 	}
 }
--- internal/command/root_test.go
+++ internal/command/root_test.go
@@ -11,18 +11,23 @@ func TestRootCMD(t *testing.T) {
 		t.Run("TOML", selectTestForParser("toml", tomlData, tomlDataSingle))
 	})
 	t.Run("PutString", func(t *testing.T) {
-		t.Run("JSON", putStringTestForParserJSON("json"))
-		t.Run("YAML", putStringTestForParserYAML("yaml"))
-		t.Run("TOML", putStringTestForParserTOML("toml"))
+		t.Run("JSON", putStringTestForParserJSON())
+		t.Run("YAML", putStringTestForParserYAML())
+		t.Run("TOML", putStringTestForParserTOML())
 	})
 	t.Run("PutInt", func(t *testing.T) {
-		t.Run("JSON", putIntTestForParserJSON("json"))
-		t.Run("YAML", putIntTestForParserYAML("yaml"))
-		t.Run("TOML", putIntTestForParserTOML("toml"))
+		t.Run("JSON", putIntTestForParserJSON())
+		t.Run("YAML", putIntTestForParserYAML())
+		t.Run("TOML", putIntTestForParserTOML())
 	})
 	t.Run("PutBool", func(t *testing.T) {
-		t.Run("JSON", putBoolTestForParserJSON("json"))
-		t.Run("YAML", putBoolTestForParserYAML("yaml"))
-		t.Run("TOML", putBoolTestForParserTOML("toml"))
+		t.Run("JSON", putBoolTestForParserJSON())
+		t.Run("YAML", putBoolTestForParserYAML())
+		t.Run("TOML", putBoolTestForParserTOML())
+	})
+	t.Run("PutObject", func(t *testing.T) {
+		t.Run("JSON", putObjectTestForParserJSON())
+		t.Run("YAML", putObjectTestForParserYAML())
+		t.Run("TOML", putObjectTestForParserTOML())
 	})
 }
