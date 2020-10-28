40006cddadee483d02b559b0e768cbf2a73871ea
Tom Wright <contact@tomwright.me>
2020-10-28 14:34:07 +0000

    Get a basic XML parser working

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/tomwright/dasel
 go 1.15
 
 require (
+	github.com/clbanning/mxj v1.8.5-0.20201012155914-b957cfd48b51
 	github.com/pelletier/go-toml v1.8.1
 	github.com/spf13/cobra v1.0.0
 	gopkg.in/yaml.v2 v2.2.2
 )
+
+replace github.com/clbanning/mxj => /Users/tom/repos/github.com/TomWright/mxj
---
+++ internal/storage/xml_test.go
@@ -0,0 +1,51 @@
+package storage_test
+
+import (
+	"github.com/tomwright/dasel/internal/storage"
+	"reflect"
+	"testing"
+)
+
+var xmlBytes = []byte(`<user>
+  <name>Tom</name>
+</user>`)
+var xmlMap = map[string]interface{}{
+	"user": map[string]interface{}{
+		"name": "Tom",
+	},
+}
+
+func TestXMLParser_FromBytes(t *testing.T) {
+	got, err := (&storage.XMLParser{}).FromBytes(xmlBytes)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	if !reflect.DeepEqual(xmlMap, got) {
+		t.Errorf("expected %v, got %v", xmlMap, got)
+	}
+}
+
+func TestXMLParser_FromBytes_Error(t *testing.T) {
+	_, err := (&storage.XMLParser{}).FromBytes(nil)
+	if err == nil {
+		t.Errorf("expected error but got none")
+		return
+	}
+	_, err = (&storage.XMLParser{}).FromBytes(yamlBytes)
+	if err == nil {
+		t.Errorf("expected error but got none")
+		return
+	}
+}
+
+func TestXMLParser_ToBytes(t *testing.T) {
+	got, err := (&storage.XMLParser{}).ToBytes(xmlMap)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+		return
+	}
+	if !reflect.DeepEqual(xmlBytes, got) {
+		t.Errorf("expected %v, got %v", string(xmlBytes), string(got))
+	}
+}
