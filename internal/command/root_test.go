bd0f874b74dcf2f669da48fbaeedc233efe7dd84
Tom Wright <contact@tomwright.me>
2020-10-28 14:04:47 +0000

    Clean up some more tests

--- internal/command/root_test.go
+++ internal/command/root_test.go
@@ -15,244 +15,130 @@ func TestChangeDefaultCommand(t *testing.T) {
 		os.Args = cachedArgs
 	}()
 
-	t.Run("ChangeToSelect", func(t *testing.T) {
-		os.Args = []string{"dasel", "-p", "json", ".name"}
-		exp := []string{"dasel", "select", "-p", "json", ".name"}
+	testArgs := func(in []string, exp []string) func(t *testing.T) {
+		return func(t *testing.T) {
+			os.Args = in
 
-		cmd := command.NewRootCMD()
-		command.ChangeDefaultCommand(cmd, "select")
+			cmd := command.NewRootCMD()
+			command.ChangeDefaultCommand(cmd, "select")
 
-		got := os.Args
-		if !reflect.DeepEqual(exp, got) {
-			t.Errorf("expected args %v, got %v", exp, got)
+			got := os.Args
+			if !reflect.DeepEqual(exp, got) {
+				t.Errorf("expected args %v, got %v", exp, got)
+			}
 		}
-	})
+	}
 
-	t.Run("AlreadySelect", func(t *testing.T) {
-		os.Args = []string{"dasel", "select", "-p", "json", ".name"}
-		exp := []string{"dasel", "select", "-p", "json", ".name"}
+	t.Run("ChangeToSelect", testArgs(
+		[]string{"dasel", "-p", "json", ".name"},
+		[]string{"dasel", "select", "-p", "json", ".name"},
+	))
 
-		cmd := command.NewRootCMD()
-		command.ChangeDefaultCommand(cmd, "select")
+	t.Run("AlreadySelect", testArgs(
+		[]string{"dasel", "select", "-p", "json", ".name"},
+		[]string{"dasel", "select", "-p", "json", ".name"},
+	))
 
-		got := os.Args
-		if !reflect.DeepEqual(exp, got) {
-			t.Errorf("expected args %v, got %v", exp, got)
-		}
-	})
-
-	t.Run("AlreadyPut", func(t *testing.T) {
-		os.Args = []string{"dasel", "put", "-p", "json", "-t", "string", "name=Tom"}
-		exp := []string{"dasel", "put", "-p", "json", "-t", "string", "name=Tom"}
-
-		cmd := command.NewRootCMD()
-		command.ChangeDefaultCommand(cmd, "select")
-
-		got := os.Args
-		if !reflect.DeepEqual(exp, got) {
-			t.Errorf("expected args %v, got %v", exp, got)
-		}
-	})
+	t.Run("AlreadyPut", testArgs(
+		[]string{"dasel", "put", "-p", "json", "-t", "string", "name=Tom"},
+		[]string{"dasel", "put", "-p", "json", "-t", "string", "name=Tom"},
+	))
 }
 
 func TestRootCMD(t *testing.T) {
+	expectErr := func(args []string, expErr string) func(t *testing.T) {
+		return func(t *testing.T) {
+			cmd := command.NewRootCMD()
+			outputBuffer := bytes.NewBuffer([]byte{})
+
+			cmd.SetOut(outputBuffer)
+			cmd.SetArgs(args)
+
+			err := cmd.Execute()
+
+			if err == nil || !strings.Contains(err.Error(), expErr) {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+		}
+	}
+	expectOutput := func(in string, args []string, exp string) func(t *testing.T) {
+		return func(t *testing.T) {
+			cmd := command.NewRootCMD()
+			outputBuffer := bytes.NewBuffer([]byte{})
+
+			cmd.SetIn(bytes.NewReader([]byte(in)))
+			cmd.SetOut(outputBuffer)
+			cmd.SetArgs(args)
+
+			err := cmd.Execute()
+
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+
+			got := outputBuffer.String()
+			if exp != got {
+				t.Errorf("expected %s, got %s", exp, got)
+			}
+		}
+	}
+
 	t.Run("Select", func(t *testing.T) {
 		t.Run("JSON", selectTestForParser("json", jsonData, jsonDataSingle))
 		t.Run("YAML", selectTestForParser("yaml", yamlData, yamlDataSingle))
 		t.Run("TOML", selectTestForParser("toml", tomlData, tomlDataSingle))
-		t.Run("InvalidFile", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			args := []string{
-				"select", "-f", "bad.json", "-s", "x",
-			}
-
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err == nil || !strings.Contains(err.Error(), "could not open input file") {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-		})
-		t.Run("MissingParser", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			args := []string{
-				"select", "-s", "x",
-			}
-
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err == nil || !strings.Contains(err.Error(), "parser flag required when reading from stdin") {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-		})
-		t.Run("Stdin", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			in := []byte(`{"name": "Tom"}`)
-			exp := `Tom
-`
-
-			args := []string{
-				"select", "-f", "stdin", "-p", "json", "-s", ".name",
-			}
-
-			cmd.SetIn(bytes.NewReader(in))
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err != nil {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-
-			got := outputBuffer.String()
-			if exp != got {
-				t.Errorf("expected %s, got %s", exp, got)
-			}
-		})
-		t.Run("StdinAlias", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			in := []byte(`{"name": "Tom"}`)
-			exp := `Tom
-`
-
-			args := []string{
-				"select", "-f", "-", "-p", "json", "-s", ".name",
-			}
-
-			cmd.SetIn(bytes.NewReader(in))
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err != nil {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-
-			got := outputBuffer.String()
-			if exp != got {
-				t.Errorf("expected %s, got %s", exp, got)
-			}
-		})
+		t.Run("InvalidFile", expectErr(
+			[]string{"select", "-f", "bad.json", "-s", "x"},
+			"could not open input file",
+		))
+		t.Run("MissingParser", expectErr(
+			[]string{"select", "-s", "x"},
+			"parser flag required when reading from stdin",
+		))
+		t.Run("Stdin", expectOutput(
+			`{"name": "Tom"}`,
+			[]string{"select", "-f", "stdin", "-p", "json", "-s", ".name"},
+			`Tom
+`,
+		))
+		t.Run("StdinAlias", expectOutput(
+			`{"name": "Tom"}`,
+			[]string{"select", "-f", "-", "-p", "json", "-s", ".name"},
+			`Tom
+`,
+		))
 	})
 	t.Run("PutString", func(t *testing.T) {
 		t.Run("JSON", putStringTestForParserJSON())
 		t.Run("YAML", putStringTestForParserYAML())
 		t.Run("TOML", putStringTestForParserTOML())
-		t.Run("InvalidFile", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
 
-			args := []string{
-				"put", "string", "-f", "bad.json", "-s", "x", "y",
-			}
-
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err == nil || !strings.Contains(err.Error(), "could not open input file") {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-		})
-		t.Run("MissingParser", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			args := []string{
-				"put", "string", "-s", "x", "y",
-			}
-
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err == nil || !strings.Contains(err.Error(), "parser flag required when reading from stdin") {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-		})
-		t.Run("StdinStdout", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			in := []byte(`{"name": "Tom"}`)
-			exp := `{
+		t.Run("InvalidFile", expectErr(
+			[]string{"put", "string", "-f", "bad.json", "-s", "x", "y"},
+			"could not open input file",
+		))
+		t.Run("MissingParser", expectErr(
+			[]string{"put", "string", "-s", "x", "y"},
+			"parser flag required when reading from stdin",
+		))
+		t.Run("StdinStdout", expectOutput(
+			`{"name": "Tom"}`,
+			[]string{"put", "string", "-f", "stdin", "-o", "stdout", "-p", "json", "-s", ".name", "Frank"},
+			`{
   "name": "Frank"
 }
-`
-
-			args := []string{
-				"put", "string", "-f", "stdin", "-o", "stdout", "-p", "json", "-s", ".name", "Frank",
-			}
-
-			cmd.SetIn(bytes.NewReader(in))
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err != nil {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-
-			got := outputBuffer.String()
-			if exp != got {
-				t.Errorf("expected %s, got %s", exp, got)
-			}
-		})
-		t.Run("StdinStdoutAlias", func(t *testing.T) {
-			cmd := command.NewRootCMD()
-			outputBuffer := bytes.NewBuffer([]byte{})
-
-			in := []byte(`{"name": "Tom"}`)
-			exp := `{
+`,
+		))
+		t.Run("StdinStdoutAlias", expectOutput(
+			`{"name": "Tom"}`,
+			[]string{"put", "string", "-f", "-", "-o", "-", "-p", "json", "-s", ".name", "Frank"},
+			`{
   "name": "Frank"
 }
-`
-
-			args := []string{
-				"put", "string", "-f", "-", "-o", "-", "-p", "json", "-s", ".name", "Frank",
-			}
-
-			cmd.SetIn(bytes.NewReader(in))
-			cmd.SetOut(outputBuffer)
-			cmd.SetArgs(args)
-
-			err := cmd.Execute()
-
-			if err != nil {
-				t.Errorf("unexpected error: %v", err)
-				return
-			}
-
-			got := outputBuffer.String()
-			if exp != got {
-				t.Errorf("expected %s, got %s", exp, got)
-			}
-		})
+`,
+		))
 	})
 	t.Run("PutInt", func(t *testing.T) {
 		t.Run("JSON", putIntTestForParserJSON())
