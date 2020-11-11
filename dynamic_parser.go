f85c93d67af5a52c51950e90b434d3c0c110e6ea
Tom Wright <contact@tomwright.me>
2020-11-11 22:33:29 +0000

    Fix an issue with dynamic selectors, caused by a regex pattern matching too many sets of brackets

--- dynamic_parser.go
+++ dynamic_parser.go
@@ -1,17 +1,38 @@
 package dasel
 
-import "errors"
+import (
+	"errors"
+)
 
 // ErrDynamicSelectorBracketMismatch is returned when the number of opening brackets doesn't equal that
 // of the closing brackets.
 var ErrDynamicSelectorBracketMismatch = errors.New("dynamic selector bracket mismatch")
+
+// ExtractNextSelector returns the next selector from the given input.
+func ExtractNextSelector(input string) string {
+	res := ""
+	i := 0
+	for k, v := range input {
+		if v == '(' || v == '[' {
+			i++
+		} else if v == ')' || v == ']' {
+			i--
+		}
+
+		if i == 0 && v == '.' && k != 0 {
+			break
+		}
+		res += string(v)
+	}
+	return res
+}
 
 // DynamicSelectorToGroups takes a dynamic selector and splits it into groups.
 func DynamicSelectorToGroups(selector string) ([]string, error) {
 	i := 0
 	tmp := ""
 	res := make([]string, 0)
-	for _, v := range selector {
+	for k, v := range selector {
 		if v == '(' {
 			if i > 0 {
 				tmp += string(v)
@@ -27,6 +48,8 @@ func DynamicSelectorToGroups(selector string) ([]string, error) {
 			} else {
 				tmp += string(v)
 			}
+		} else if v == '.' && i == 0 && k != 0 {
+			return res, nil
 		} else {
 			tmp += string(v)
 		}
