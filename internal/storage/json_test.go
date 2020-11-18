2621c3301649985ea1ace1d6c205596e236559ad
Tom Wright <contact@tomwright.me>
2020-11-18 18:52:40 +0000

    Add tests

--- internal/storage/json_test.go
+++ internal/storage/json_test.go
@@ -33,26 +33,20 @@ func TestJSONParser_FromBytes(t *testing.T) {
 			return
 		}
 		exp := &storage.JSONMultiDocument{
-			Values: []interface{}{
-				map[string]interface{}{"name": "Tom"},
-				map[string]interface{}{"name": "Ellis"},
-			},
+			Values: jsonMapMulti,
 		}
 		if !reflect.DeepEqual(exp, got) {
 			t.Errorf("expected %v, got %v", jsonMap, got)
 		}
 	})
 	t.Run("ValidMultiDocumentMixed", func(t *testing.T) {
-		got, err := (&storage.JSONParser{}).FromBytes(mixedJsonBytesMulti)
+		got, err := (&storage.JSONParser{}).FromBytes(jsonBytesMultiMixed)
 		if err != nil {
 			t.Errorf("unexpected error: %s", err)
 			return
 		}
 		exp := &storage.JSONMultiDocument{
-			Values: []interface{}{
-				map[string]interface{}{"name": "Tom", "other": true},
-				map[string]interface{}{"name": "Ellis"},
-			},
+			Values: jsonMapMultiMixed,
 		}
 		if !reflect.DeepEqual(exp, got) {
 			t.Errorf("expected %v, got %v", jsonMap, got)
@@ -79,18 +73,52 @@ func TestJSONParser_FromBytes_Error(t *testing.T) {
 }
 
 func TestJSONParser_ToBytes(t *testing.T) {
-	got, err := (&storage.JSONParser{}).ToBytes(jsonMap)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-		return
-	}
-	if string(jsonBytes) != string(got) {
-		t.Errorf("expected %v, got %v", string(jsonBytes), string(got))
-	}
+	t.Run("Valid", func(t *testing.T) {
+		got, err := (&storage.JSONParser{}).ToBytes(jsonMap)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			return
+		}
+		if string(jsonBytes) != string(got) {
+			t.Errorf("expected %v, got %v", string(jsonBytes), string(got))
+		}
+	})
+
+	t.Run("ValidSingle", func(t *testing.T) {
+		got, err := (&storage.JSONParser{}).ToBytes(&storage.JSONSingleDocument{Value: jsonMap})
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			return
+		}
+		if string(jsonBytes) != string(got) {
+			t.Errorf("expected %v, got %v", string(jsonBytes), string(got))
+		}
+	})
+
+	t.Run("ValidMulti", func(t *testing.T) {
+		got, err := (&storage.JSONParser{}).ToBytes(&storage.JSONMultiDocument{Values: jsonMapMulti})
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			return
+		}
+		if string(jsonBytesMulti) != string(got) {
+			t.Errorf("expected %v, got %v", string(jsonBytesMulti), string(got))
+		}
+	})
+
+	t.Run("ValidMultiMixed", func(t *testing.T) {
+		got, err := (&storage.JSONParser{}).ToBytes(&storage.JSONMultiDocument{Values: jsonMapMultiMixed})
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			return
+		}
+		if string(jsonBytesMultiMixed) != string(got) {
+			t.Errorf("expected %v, got %v", string(jsonBytesMultiMixed), string(got))
+		}
+	})
 }
 
-var jsonBytesMulti = []byte(`
-{
+var jsonBytesMulti = []byte(`{
   "name": "Tom"
 }
 {
@@ -98,8 +126,12 @@ var jsonBytesMulti = []byte(`
 }
 `)
 
-var mixedJsonBytesMulti = []byte(`
-{
+var jsonMapMulti = []interface{}{
+	map[string]interface{}{"name": "Tom"},
+	map[string]interface{}{"name": "Ellis"},
+}
+
+var jsonBytesMultiMixed = []byte(`{
   "name": "Tom",
   "other": true
 }
@@ -107,3 +139,8 @@ var mixedJsonBytesMulti = []byte(`
   "name": "Ellis"
 }
 `)
+
+var jsonMapMultiMixed = []interface{}{
+	map[string]interface{}{"name": "Tom", "other": true},
+	map[string]interface{}{"name": "Ellis"},
+}
