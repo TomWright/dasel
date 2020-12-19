f4df9c93a72ffe11ec8d7cdaf314853a50841a07
Tom Wright <contact@tomwright.me>
2020-12-19 22:44:39 +0000

    Add some tests

---
+++ internal/command/put_document_internal_test.go
@@ -0,0 +1,56 @@
+package command
+
+import (
+	"errors"
+	"github.com/tomwright/dasel/internal/storage"
+	"strings"
+	"testing"
+)
+
+func TestPut_Document(t *testing.T) {
+	t.Run("SingleFailingWriter", func(t *testing.T) {
+		err := runPutDocumentCommand(putDocumentOpts{
+			Parser:         "json",
+			Selector:       ".[0]",
+			Reader:         strings.NewReader(`[{"name": "Tom"}]`),
+			DocumentString: `{"name": "Frank"}`,
+			Writer:         &failingWriter{},
+		}, nil)
+
+		if err == nil || !errors.Is(err, errFailingWriterErr) {
+			t.Errorf("expected error %v, got %v", errFailingWriterErr, err)
+			return
+		}
+	})
+	t.Run("MultiFailingWriter", func(t *testing.T) {
+		err := runPutDocumentCommand(putDocumentOpts{
+			Parser:         "json",
+			Selector:       ".[*]",
+			Reader:         strings.NewReader(`[{"name": "Tom"}]`),
+			DocumentString: `{"name": "Frank"}`,
+			Writer:         &failingWriter{},
+			Multi:          true,
+		}, nil)
+
+		if err == nil || !errors.Is(err, errFailingWriterErr) {
+			t.Errorf("expected error %v, got %v", errFailingWriterErr, err)
+			return
+		}
+	})
+	t.Run("InvalidDocumentParser", func(t *testing.T) {
+		err := runPutDocumentCommand(putDocumentOpts{
+			Parser:         "json",
+			Selector:       ".[*]",
+			Reader:         strings.NewReader(`[{"name": "Tom"}]`),
+			DocumentString: `{"name": "Frank"}`,
+			DocumentParser: "bad",
+		}, nil)
+
+		exp := &storage.UnknownParserErr{Parser: "bad"}
+
+		if err == nil || !strings.HasSuffix(err.Error(), exp.Error()) {
+			t.Errorf("expected error %v, got %v", exp, err)
+			return
+		}
+	})
+}
