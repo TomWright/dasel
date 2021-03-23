c08d3f2ba85fa375baaaec2e736dd5105ab0f540
Tom Wright <contact@tomwright.me>
2021-03-23 21:16:48 +0000

    Automatically add build version

--- internal/version.go
+++ internal/version.go
@@ -1,5 +1,31 @@
 package internal
 
+import (
+	"runtime/debug"
+)
+
 // Version represents the current version of dasel.
 // The real version number is injected at build time using ldflags.
 var Version = "development"
+
+func init() {
+	// Version is set by ldflags on build.
+	if Version != "development" {
+		return
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	// https://github.com/golang/go/issues/29228
+	if info.Main.Version == "(devel)" {
+		return
+	}
+
+	Version = info.Main.Version
+	if len(info.Main.Sum) >= 7 {
+		Version += "-" + info.Main.Version[0:6]
+	}
+}
