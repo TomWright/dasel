1ef5d228b417109e9749cb8ee301fc891545c236
Tom Wright <contact@tomwright.me>
2025-08-23 19:57:49 +0100

    Remove unused code

--- selector/parser/parse_variable.go
+++ selector/parser/parse_variable.go
@@ -2,7 +2,6 @@ package parser
 
 import (
 	"github.com/tomwright/dasel/v3/selector/ast"
-	"github.com/tomwright/dasel/v3/selector/lexer"
 )
 
 func parseVariable(p *Parser) (ast.Expr, error) {
@@ -14,33 +13,10 @@ func parseVariable(p *Parser) (ast.Expr, error) {
 
 	p.advance()
 
-	//if p.current().IsKind(lexer.Equals) {
-	//	return parseAssignment(p, prop)
-	//}
-
 	res, err := parseFollowingSymbol(p, prop)
 	if err != nil {
 		return nil, err
 	}
-
-	return res, nil
-}
-
-func parseAssignment(p *Parser, variableExpr ast.VariableExpr) (ast.Expr, error) {
-	if err := p.expect(lexer.Equals); err != nil {
-		return nil, err
-	}
-	p.advance()
-
-	res := ast.AssignExpr{
-		Variable: variableExpr,
-	}
-
-	valueExpr, err := p.parseExpressions(lexer.TokenKinds(lexer.Semicolon, lexer.CloseParen), nil, true, bpAssignment, true)
-	if err != nil {
-		return nil, err
-	}
-	res.Value = valueExpr
 
 	return res, nil
 }
